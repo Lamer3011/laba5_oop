8e1f47d4d08b7961228621714d23e5ad3d2fed90
Lamer Matrixxx <jssk383@gmail.com>
2021-04-07 12:06:06 +0300

    Добавив 45 вправу

--- ex45/Program.cs
+++ ex45/Program.cs
@@ -6,7 +6,19 @@ namespace ex45
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Was:");
+            string s = "  Two    words  string    to lace   from the ceiling  ";
+            Console.WriteLine(s);
+            s = s.Trim();
+            while (s.Contains("  "))
+                s = s.Replace("  ", " ");
+            string[] attString = s.Split(' ');
+            Array.Sort(attString);
+            string sNew = string.Join(' ', attString);
+            Console.WriteLine();
+            Console.WriteLine("Became:");
+            Console.WriteLine(sNew);
+            Console.ReadKey();
         }
     }
 }
